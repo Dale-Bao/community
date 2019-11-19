ae58816647cd42748763d793a7aa0e6d35a8dae5
Dale <bao193438@163.com>
2019-11-19 17:41:45 +0800

    flyway 暂未成功

--- src/main/java/dale/springboot/mapper/UserMapper.java
+++ src/main/java/dale/springboot/mapper/UserMapper.java
@@ -3,6 +3,8 @@ package dale.springboot.mapper;
 import dale.springboot.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 /**
  * @author xueneng on 2019/11/12.
@@ -12,4 +14,7 @@ import org.apache.ibatis.annotations.Mapper;
 public interface UserMapper {
     @Insert("INSERT INTO user (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
+
+    @Select("SELECT * FROM USER WHERE TOKEN = #{token}")
+    User findByToken(@Param("token") String token);
 }
