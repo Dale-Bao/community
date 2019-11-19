a404905d0d00ad3b014be12afa30ba8b78634bb9
Dale <bao193438@163.com>
2019-11-19 16:26:45 +0800

    浏览器储存用户token

--- src/main/java/dale/springboot/controller/IndexController.java
+++ src/main/java/dale/springboot/controller/IndexController.java
@@ -1,16 +1,39 @@
 package dale.springboot.controller;
 
+import dale.springboot.mapper.UserMapper;
+import dale.springboot.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * @author xueneng on 2019/11/5.
- *         Description:
+ * Description:
  */
 @Controller
 public class IndexController {
+
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String index(){
+    public String index(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("token")) {
+                    String token = cookie.getValue();
+                    User user = userMapper.findByToken(token);
+                    if (user != null) {
+                        request.getSession().setAttribute("user", user);
+                    }
+                    break;
+                }
+            }
+        }
         return "index";
     }
 }
