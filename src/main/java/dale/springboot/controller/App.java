a685a6f57a8d96594312ee18efb67a6da16a27bb
Dale <bao193438@163.com>
2019-11-20 15:31:22 +0800

    flyway 成功

--- src/main/java/dale/springboot/controller/App.java
+++ src/main/java/dale/springboot/controller/App.java
@@ -8,7 +8,8 @@ import org.flywaydb.core.Flyway;
  */
 public class App {
     public static void main(String[] args) {
-        Flyway flyway = Flyway.configure().dataSource("jdbc:mysql://localhost:3306/community","root","123456").load();
+        Flyway flyway = Flyway.configure().dataSource("jdbc:mysql://localhost:3306/community?serverTimezone=UTC","root","123456").load();
+        flyway.baseline();
         //start the migration
         flyway.migrate();
     }
---
+++ src/main/resources/db/migration/V2__addDate.sql
@@ -0,0 +1,36 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 80018
+ Source Host           : localhost:3306
+ Source Schema         : community
+
+ Target Server Type    : MySQL
+ Target Server Version : 80018
+ File Encoding         : 65001
+
+ Date: 19/11/2019 16:51:10
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `bio` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `token` char(36) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `create_d` date NULL DEFAULT NULL,
+  `account_id` varbinary(100) NULL DEFAULT NULL,
+  `gmt_create` bigint(19) NULL DEFAULT NULL,
+  `gmt_modified` bigint(19) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
